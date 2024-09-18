9c28927fbb18a3066c67b9b59ffbb1c41adff139
Rakib <hellorakibhossen@gmail.com>
2024-09-18 08:03:44 -0700

    first commit

---
+++ js/login.js
@@ -0,0 +1,9 @@
+document
+  .getElementById("button-login")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("login Button Click");
+
+    const phoneNumber = document.getElementById("phone-number").value;
+    console.log(phoneNumber);
+  });
