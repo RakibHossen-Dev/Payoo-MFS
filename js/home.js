4c5cacf266d6a728db39b18e4cf8413b4894aefb
Rakib <hellorakibhossen@gmail.com>
2024-09-18 20:15:19 -0700

    home page done

--- js/home.js
+++ js/home.js
@@ -8,4 +8,16 @@ document
 
     const pinNumberInput = document.getElementById("input-pin-number").value;
     console.log(pinNumberInput);
+
+    if (pinNumberInput === "1234") {
+      console.log("adding money to your account");
+      const blance = document.getElementById("account-blance").innerText;
+      const addMoneyNumber = parseFloat(addMoneyInput);
+      const blanceNumber = parseFloat(blance);
+      const newBlance = addMoneyNumber + blanceNumber;
+      console.log(newBlance);
+      document.getElementById("account-blance").innerText = newBlance;
+    } else {
+      alert("Failed to add money! please to try again");
+    }
   });
